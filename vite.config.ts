aabdbad4c821deabd284344bf854002e9c690c6b
goreckis <slavomir.gorecki@gmail.com>
2026-01-13 11:58:35 +0100

    Fix: use correct nodeAdapter import path for Qwik 1.5+

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,24 @@ import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nodeAdapter } from "./adapters";
 
-export default defineConfig({
-  plugins: [
-    qwikCity({}),
-    qwikVite(),
-    tsconfigPaths()
-  ],
-  preview: {
-    headers: {
-      "Cache-Control": "public, max-age=600",
+export default defineConfig(() => {
+  return {
+    plugins: [
+      qwikCity({
+        adapter: nodeAdapter(),
+      }),
+      qwikVite(),
+      tsconfigPaths()
+    ],
+    preview: {
+      headers: {
+        "Cache-Control": "public, max-age=600",
+      },
     },
-  },
-  server: {
-    port: 5173,
-  },
+    server: {
+      port: 5173,
+    },
+  };
 });
