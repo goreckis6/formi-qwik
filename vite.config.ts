5e76775c570e9971e52265c9e954a2445c22a720
goreckis <slavomir.gorecki@gmail.com>
2026-01-13 13:58:58 +0100

    Fix SSR build: simple config without adapter, qwik build + build.server script

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from "vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikCity } from "@builder.io/qwik-city/vite";
-import { nodeServerAdapter } from "@builder.io/qwik-city/adapters/node-server/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => ({
   plugins: [
-    qwikCity({
-      adapter: nodeServerAdapter({
-        ssg: null, // Wyłącz SSG, tylko SSR
-      }),
-    }),
+    qwikCity(),
     qwikVite(),
     tsconfigPaths(),
   ],
